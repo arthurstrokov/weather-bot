3952ead00b3bb2e7e71f68ec49a34be5f4dc2e33
arthurstrokov <arthurstrokov@gmail.com>
2022-09-26 12:39:53 +0300

    set buttons in telegram

--- src/main/java/com/gmail/arthurstrokov/weatherbot/service/WeatherForTomorrowBotService.java
+++ src/main/java/com/gmail/arthurstrokov/weatherbot/service/WeatherForTomorrowBotService.java
@@ -7,8 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -28,28 +35,46 @@ public class WeatherForTomorrowBotService extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            long chatId = update.getMessage().getChatId();
-            if (update.getMessage().getText().equals("/start")) {
+        Message message = update.getMessage();
+        if (update.hasMessage() && message.hasText()) {
+            if (message.getText().equals("/start")) {
                 String weatherForecastData = openWeatherApiService.getWeatherForecastData();
-                sendMsg(chatId, weatherForecastData);
+                sendMsg(message, weatherForecastData);
             } else if (update.getMessage().getText().equals("/test")) {
                 String currentWeather = openWeatherApiService.getCurrentWeather();
-                sendMsg(chatId, currentWeather);
+                sendMsg(message, currentWeather);
             }
         }
     }
 
-    private void sendMsg(Long chatId, String text) {
-        SendMessage message = SendMessage.builder() // Create a message object
-                .chatId(chatId)
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = SendMessage.builder() // Create a message object
+                .chatId(message.getChatId())
                 .text(text)
                 .build();
         try {
-            execute(message); // Sending our message object to user
+            setButton(sendMessage);
+            execute(sendMessage); // Sending our message object to user
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    public void setButton(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.enableHtml(true);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        keyboardFirstRow.add(new KeyboardButton("/start"));
+        keyboardFirstRow.add(new KeyboardButton("/test"));
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
     }
 
     @Override

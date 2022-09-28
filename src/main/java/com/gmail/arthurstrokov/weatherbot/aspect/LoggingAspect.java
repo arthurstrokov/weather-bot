e55e44b84051641a22cde4771303c97f25a524f0
arthurstrokov <arthurstrokov@gmail.com>
2022-09-28 09:44:14 +0300

    up aop logic

--- src/main/java/com/gmail/arthurstrokov/weatherbot/aspect/LoggingAspect.java
+++ src/main/java/com/gmail/arthurstrokov/weatherbot/aspect/LoggingAspect.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import java.util.Arrays;
 
@@ -26,14 +27,35 @@ public class LoggingAspect {
         // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
 
+    @Pointcut("execution(* com.gmail.arthurstrokov.weatherbot.service.*.*(..))" +
+            "&& !@annotation(com.gmail.arthurstrokov.weatherbot.aspect.NoLogging)" +
+            "&& !@target(com.gmail.arthurstrokov.weatherbot.aspect.NoLogging)")
+    public void servicePointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    @Pointcut("execution(* com.gmail.arthurstrokov.weatherbot.controller.*.*(..))")
+    public void controllerPointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
     @AfterThrowing(pointcut = "applicationPointcut()", throwing = "e")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
         log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
                 joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
     }
 
-    @Around("applicationPointcut()")
-    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("servicePointcut()")
+    public Object logAroundService(ProceedingJoinPoint joinPoint) throws Throwable {
+        return logAround(joinPoint);
+    }
+
+    @Around("controllerPointcut()")
+    public Object logAroundController(ProceedingJoinPoint joinPoint) throws Throwable {
+        return logAround(joinPoint);
+    }
+
+    private Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
         if (log.isDebugEnabled()) {
             log.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
                     joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
@@ -50,5 +72,15 @@ public class LoggingAspect {
                     joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
             throw e;
         }
+    }
+
+    @Around("applicationPointcut()")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        final StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Object proceed = joinPoint.proceed();
+        stopWatch.stop();
+        log.info("{} executed in {} ms", joinPoint.getSignature(), stopWatch.getTotalTimeMillis());
+        return proceed;
     }
 }

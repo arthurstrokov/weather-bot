2baffdd334bd32a55ea8aa167fb9a8655971e249
Строков Артур Александрович <AAStrokov@alfabank.ru>
2025-10-22 12:07:15 +0300

    updated build.gradle

--- src/test/java/com/gmail/arthurstrokov/weather/controller/WeatherControllerTest.java
+++ src/test/java/com/gmail/arthurstrokov/weather/controller/WeatherControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.wiremock.spring.EnableWireMock;
 
@@ -25,7 +24,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @EnableWireMock
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-@ActiveProfiles("test")
 class WeatherControllerTest {
 
     private final MockMvc mockMvc;
